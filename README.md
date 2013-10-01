b33185217a8fd442edfc4fb72d4d3e6382b99cb1
rutgerkok <rutger.kok@live.com>
2013-10-01 09:37:02 +0200

    Start conversion of ForgeJarCrator to a Maven plugin

--- README.md
+++ README.md
@@ -2,10 +2,18 @@
 
 This repo will contain some tools to assist people compiling mods for Minecraft Forge.
 
-### ForgeJarCreator
-This little program downloads Minecraft, Forge and the MCP mappings and outputs a .jar file.
-The jar file only contains empty methods, so it should be safe to redistribute.
+*This project is in no way affliated with Forge or the Forge team.*
 
-Forge mods need to add this to their build path, either manually or using Maven. (For now Maven doesn't work yet.)
+### ForgeJars
+ForgeJars contains one file: the `pom.xml` file. In this file, all settings are present to generate
+the Forge jar using ForgeJarCreator. The resulting jar file contains only empty methods, so it
+should be safe to build against. The file won't run.
+
+### ForgeJarCreator
+This program does the actual work. It downloads Minecraft, Forge and the MCP mappings and outputs
+a .jar file. It needs to be run as a Maven plugin.
+
+ForgeJarCreator relies on [SpecialSource](https://github.com/md-5/SpecialSource), 
+[Minecraft Forge](http://www.minecraftforge.net/) and the [MCP Mappings](http://mcp.ocean-labs.de/).
 
 ForgeJarCreator contains a modified version of [Javaxdelta](http://javaxdelta.sourceforge.net/).
--- forgejarcreator/src/main/java/nl/rutgerkok/forgejarcreator/Main.java
+++ forgejarcreator/src/main/java/nl/rutgerkok/forgejarcreator/Main.java
@@ -5,28 +5,34 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.zip.ZipFile;
 
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+
 import net.md_5.specialsource.util.FileLocator;
 import nl.rutgerkok.forgejarcreator.patch.ForgePatcher;
 import nl.rutgerkok.forgejarcreator.patch.MinecraftJar;
 
 import com.google.common.io.Files;
 
-public class Main {
+@Mojo(name = "forgejarcreator")
+public class Main extends AbstractMojo {
 
     /**
-     * Single-threaded commandline application to create a file for Forge mods
-     * to build against.
-     * <p>
-     * First, the needed files are downloaded, then they are renamed, then the
-     * missing methods added by Forge are added and finally all method
-     * implementations are removed, making the outputted files safer to redistribute.
-     * 
-     * @param args
-     *            Ignored.
-     * @throws IOException
-     *             If something goes wrong. (Usually network problems.)
+     * Downloads the jars, patches them with the binary patches by Forge,
+     * removes all method implementations and renames the classes, fields and
+     * methods. Result: a Forge jar for people to build against.
      */
-    public static void main(String[] args) throws IOException {       
+    public void execute() throws MojoExecutionException {
+        try {
+            execute0();
+        } catch (IOException e) {
+            // Convert exception
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+    }
+
+    private void execute0() throws IOException {
         // Download
         File clientDownloadedJar = FileLocator.getFile(Constants.MINECRAFT_CLIENT_URL);
         // File serverJar = FileLocator.getFile(Constants.MINECRAFT_SERVER_URL);
@@ -36,7 +42,7 @@ public class Main {
         // Move to other file
         File clientTemporaryJarFile = new File("clienttemp.jar");
         Files.copy(clientDownloadedJar, clientTemporaryJarFile);
-        
+
         // Load mappings
         MCPDeobfuscator mappings = new MCPDeobfuscator();
 
@@ -49,7 +55,7 @@ public class Main {
         patchesStream.close();
         clientRenamedJar.writeToFile(clientTemporaryJarFile);
         System.out.println("Applied patches by Forge!");
-        
+
         // Rename fields, methods and classes in file
         mappings.deobfuscate(forgeJar, clientTemporaryJarFile, new File("client.jar"));
         System.err.println("But for us this is normal, as both the Forge and Minecraft jars have a Main class.");
